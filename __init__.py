01c3e09df232727cd5982c4746e62e2b4d649186
zen <zen@zenen.space>
2023-11-07 21:20:33 -0500

    cleaning

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 from . import frontmatter
-from .frontmatter.default_handlers import JSONHandler
+from .frontmatter.default_handlers import JSONHandler, YAMLHandler
 
 # Files are the basic unit where tasks are stored. Other
 # ingredients make use of the methods in this file to 
@@ -14,16 +14,16 @@ def load(path : str) -> frontmatter.Task:
 
 def load_dir(dir_path : str, sort_by=None, first=None, last=None) -> list[frontmatter.Task]:
     items = []
-    directory = os.listdir(dir_path)
+    directory = os.scandir(dir_path)
 
     for item in directory:
-        # We skip .swp files, they're yucky
-        if item.endswith('.swp'):
+        # We skip .swp files and dirs. they're yucky
+        if item.name.endswith('.swp') or item.is_dir():
             continue
 
-        with open(f'{dir_path}/{item}') as file:
+        with open(f'{item.path}') as file:
             task = frontmatter.load(file)
-            task['filename'] = item
+            task['filename'] = item.name
             items.append(task)
 
     if sort_by is not None:
@@ -37,11 +37,12 @@ def load_dir(dir_path : str, sort_by=None, first=None, last=None) -> list[frontm
 def make_file(file_dict : dict, file_contents: str, file_path : str) -> None:
     new_file = frontmatter.Task(file_contents, **file_dict)
 
-    # Making sure that the file exists before we write to it
+    # Making sure that the filepath exists before we write to it
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     open(f'{file_path}', 'x').close()
 
     with open(f'{file_path}', 'w') as file:
-        file.write(frontmatter.dumps(new_file, JSONHandler()))
+        file.write(frontmatter.dumps(new_file, YAMLHandler()))
 
 def update_file(updates : dict, file_path : str) -> None:
     task = load(file_path)
@@ -49,7 +50,7 @@ def update_file(updates : dict, file_path : str) -> None:
         task[update] = updates[update]
 
     with open(f'{file_path}', 'w') as file:
-        file.write(frontmatter.dumps(task, JSONHandler()))
+        file.write(frontmatter.dumps(task, YAMLHandler()))
 
 def delete_file(file_path : str) -> None:
     os.remove(file_path)
